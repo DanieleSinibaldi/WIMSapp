e3276da779be446879a920e461734b6befd07a9b
DanieleSinibaldi <daniele.sinibaldi@hotmail.it>
2018-09-18 10:25:28 +0200

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'rooms#places'
   resources :rooms, only: [:show, :update]
   get '/places' => "rooms#places"
   get '/places/:a_place' => "rooms#a_place"
